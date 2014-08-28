b46039b496da7eaf137b1ce804310de7ac485777
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-08-28 11:01:37 +0200

    Added build tools.

---
+++ build/Build.bat
@@ -0,0 +1,8 @@
+SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
+SET Arbor.X.Vcs.Branch.Name=develop
+SET Version.Major=0
+SET Version.Minor=1
+SET Version.Patch=3
+SET Version.Build=1
+
+CALL Build.exe
