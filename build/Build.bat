4619c43ea09b1d0f4945b5c52c6ce5cc7a1570ef
Niklas Lundberg <info@nlundberg.se>
2017-03-08 23:26:17 +0100

    Removed MSBuild specific version in build script.

--- build/Build.bat
+++ build/Build.bat
@@ -1,7 +1,5 @@
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
-SET Arbor.X.Tools.External.MSBuild.MaxVersion=14.0
 SET Arbor.X.Tools.External.VisualStudio.Version.PreRelease.Enabled=false
-SET Arbor.X.Tools.External.VisualStudio.Version=14.0
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 
 CALL "%~dp0Build.exe"
