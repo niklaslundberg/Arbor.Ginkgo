329777f2633ef65ed7061410c4da8b2ac5a3169b
Niklas Lundberg <info@nlundberg.se>
2018-08-27 22:16:50 +0200

    Update NuGet packages.

--- build/Build.bat
+++ build/Build.bat
@@ -4,6 +4,6 @@ SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 SET Arbor.X.Tools.External.VisualStudio.Version.PreRelease.Enabled=false
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 
-CALL "%~dp0Build.exe"
+CALL dotnet arbor-build
 
 EXIT /B %ERRORLEVEL%
