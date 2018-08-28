1edaaed3337257ca3f71595b6c4839f83b7b322c
Niklas Lundberg <info@nlundberg.se>
2018-08-28 09:41:50 +0200

    Added codedom to test project.

--- build/Build.bat
+++ build/Build.bat
@@ -2,7 +2,6 @@
 
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 SET Arbor.X.Tools.External.VisualStudio.Version.PreRelease.Enabled=false
-SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 
 CALL dotnet arbor-build
 
