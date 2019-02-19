a2f2ac7259361d5d0c0cb986204fac0a47712db8
Niklas Lundberg <info@nlundberg.se>
2019-02-19 10:48:45 +0100

    Enabled NuGet restorer.

--- build/Build.bat
+++ build/Build.bat
@@ -2,6 +2,7 @@
 
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 SET Arbor.X.Tools.External.VisualStudio.Version.PreRelease.Enabled=false
+SET Arbor.X.Tools.External.NuGet.Restore.Enabled=true
 
 CALL dotnet arbor-build
 
