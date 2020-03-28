7d9f226ea297065682f2fd3a1f4a0ba09e57c284
Niklas Lundberg <info@nlundberg.se>
2020-03-28 06:17:14 +0100

    Added GitHub Actions support.

--- build/Build.bat
+++ build/Build.bat
@@ -3,6 +3,8 @@
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 SET Arbor.X.Tools.External.VisualStudio.Version.PreRelease.Enabled=false
 
+SET Arbor.Build.Vcs.Branch.Name=%GITHUB_REF%
+
 CALL dotnet arbor-build
 
 EXIT /B %ERRORLEVEL%
